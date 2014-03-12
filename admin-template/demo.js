9f60551a2bffb24cc59687a8bf4568534dadba2d
larissaleite <larissa.leite@gmail.com>
2014-03-11 21:45:40 -0300

    Datepicker added

--- admin-template/demo.js
+++ admin-template/demo.js
@@ -1,21 +1,43 @@
 var app = angular.module('sysFinance', []);
 
-app.controller('addExpensesController', function($scope) {
+app.controller('addExpensesController', function($scope, storageService) {
 	$scope.destArray = [
-		{name:'Subway'},
-		{name:'McDonalds'}	
+		/*{name:'Subway'},
+		{name:'McDonalds'}	*/
 	];
+
+	if (storageService.get("destArray") != null) {
+		$scope.destArray = storageService.get("destArray");
+	}
+
 
 	$scope.dest = $scope.destArray[0];
 
 	$scope.addToSelectBox = function(destinationText) {
-		$scope.destArray = $scope.destArray.concat([{name:destinationText}]);
+
+		if (storageService.get("destArray") == null) {
+			var jsonArray = [{name:destinationText}];
+			storageService.save("destArray", jsonArray);
+		} else {
+			var jsonArray = {name:destinationText};
+			storageService.edit("destArray", jsonArray);
+		}
+		//$scope.destArray = $scope.destArray.concat([{name:destinationText}]);
+		$scope.destArray = storageService.get("destArray");
+		$scope.dest = $scope.destArray[$scope.destArray.length-1];
 	}
+
+
+	expenses = [{date:'date', item:'itemName', amount:'amount', destination:'destination', source:'source'}, {...}];
+
+});
+
+app.controller('registrationController', function($scope) {
 
 });
 
 app.directive('selectPicker', function() {
-	return function(scope, element, attributes){
+	return function(scope, element, attributes, storageService){
         element.selectpicker();
 
         scope.$watch(function () {
@@ -31,4 +53,32 @@ app.directive('selectPicker', function() {
           element.selectpicker('refresh');
         }, true);
     }
+});
+
+app.factory('storageService', function ($rootScope) {
+
+    return {
+        
+        get: function (key) {
+           return JSON.parse(localStorage.getItem(key));
+        },
+
+        edit: function(key, data) {
+            var array = JSON.parse(localStorage.getItem(key));
+            array.push(data);
+            localStorage.setItem(key, JSON.stringify(array));    
+        },
+
+        save: function (key, data) {
+           localStorage.setItem(key, JSON.stringify(data));
+        },
+
+        remove: function (key) {
+            localStorage.removeItem(key);
+        },
+        
+        clearAll : function () {
+            localStorage.clear();
+        }
+    };
 });
